e237cbc947f51cb430e18ecf4cca5195254c29c8
mwc <1819293933@qq.com>
2019-02-09 18:34:38 +0800

    订单，banner，和mixin的抽离

---
+++ src/views/layout/banner-detail.vue
@@ -0,0 +1,34 @@
+<style lang="scss" scoped>
+  .bannerDetail {
+  }
+</style>
+
+<template lang="pug">
+  .bannerDetail bannerDetail
+</template>
+
+<script>
+  import {mapState, mapGetters, mapMutations} from 'vuex'
+
+  export default {
+    name: "bannerDetail",
+    data() {
+      return {}
+    },
+    computed: {
+      ...mapState([]),
+      ...mapGetters([]),
+    },
+    methods: {
+      ...mapMutations([]),
+    },
+    created() {
+
+    },
+    mounted() {
+
+    }
+  }
+</script>
+
+
---
+++ src/views/layout/order.vue
@@ -0,0 +1,34 @@
+<style lang="scss" scoped>
+  .order {
+  }
+</style>
+
+<template lang="pug">
+  .order order
+</template>
+
+<script>
+  import {mapState, mapGetters, mapMutations} from 'vuex'
+
+  export default {
+    name: "order",
+    data() {
+      return {}
+    },
+    computed: {
+      ...mapState([]),
+      ...mapGetters([]),
+    },
+    methods: {
+      ...mapMutations([]),
+    },
+    created() {
+
+    },
+    mounted() {
+
+    }
+  }
+</script>
+
+
