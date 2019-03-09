c536f62b770ef7d1a6ea88d744fca501260ffb7e
mwc <1819293933@qq.com>
2019-03-09 22:54:24 +0800

    增加了反馈

---
+++ src/views/layout/order-detail.vue
@@ -0,0 +1,34 @@
+<style lang="scss" scoped>
+  .orderDetail {
+  }
+</style>
+
+<template lang="pug">
+  .orderDetail orderDetail
+</template>
+
+<script>
+  import {mapState, mapGetters, mapMutations} from 'vuex'
+
+  export default {
+    name: "orderDetail",
+    data() {
+      return {}
+    },
+    computed: {
+      ...mapState([]),
+      ...mapGetters([]),
+    },
+    methods: {
+      ...mapMutations([]),
+    },
+    created() {
+
+    },
+    mounted() {
+
+    }
+  }
+</script>
+
+
